32f8e64d98a0e02c6a0cb55dc4d5771b74c4817e
d-enk <d-enk@ya.ru>
2020-07-23 17:28:04 +0300

    v300

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/d-enk/gomod_test/v2
+module github.com/d-enk/gomod_test/v3
 
 go 1.14
--- lib.go
+++ lib.go
@@ -3,5 +3,5 @@ package gomod_test
 import "fmt"
 
 func Hi() {
-	fmt.Println("github v201")
+	fmt.Println("github v300")
 }
